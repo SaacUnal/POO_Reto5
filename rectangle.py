4284766bc7b9aac3cfc8df1ff95368d89384ccc5
SaacUnal <sealarcon@unal.edu.co>
2025-01-10 00:35:03 +0100

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,28 @@
+import shape
+class Rectangle(Shape):
+  def __init__(self, vertices:list[Point]):
+    super().__init__(is_regular=False)
+
+  def compute_area(self):
+    area = self.edges[0].length * self.edges[1].length
+    return area
+
+  def compute_perimeter(self):
+    perimeter = (self.edges[0].length*2) + (self.edges[1].length*2)
+    return perimeter
+
+  def compute_inner_angle(self):
+    inner_angle = 0
+    return inner_angle
+
+class Square(Rectangle):
+  def __init__(self, vertices:list[Point]):
+    super().__init__(is_regular=True)
+
+  def compute_area(self):
+    area = self.edges[0].length ** 2
+    return area
+
+  def compute_perimeter(self):
+    perimeter = self.edges[0].length * 4
+    return perimeter
