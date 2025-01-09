2a103489fdfacb20a870631bf61e3ea626622f56
SaacUnal <sealarcon@unal.edu.co>
2025-01-10 00:43:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+from rectangle import Rectangle
+from triangle import Triangle
 
+if __name__ == "__main__":
+  square = Square()
+  print(square.compute_area())
+  print(square.compute_perimeter())
+  #print(square.compute_inner_angle())
+
+  print("#-------------------------------------------------------------------")
+
+  rectangle = Rectangle()
+  print(rectangle.compute_area())
+  print(rectangle.compute_perimeter())
+  #print(rectangle.compute_inner_angle())
+
+  print("#-------------------------------------------------------------------")
+
+  equilateral = Equilateral()
+  print(equilateral.compute_area())
+  print(equilateral.compute_perimeter())
+  #print(equilateral.compute_inner_angle())
+
+  print("#-------------------------------------------------------------------")
+
+  isosceles = Isosceles()
+  print(isosceles.compute_area())
+  print(isosceles.compute_perimeter())
+  #print(isosceles.compute_inner_angle())
+
+  print("#-------------------------------------------------------------------")
+
+  scalene = Scalene()
+  print(scalene.compute_area())
+  print(scalene.compute_perimeter())
+  #print(scalene.compute_inner_angle())
